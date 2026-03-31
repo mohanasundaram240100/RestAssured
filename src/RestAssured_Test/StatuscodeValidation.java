94d7b2d318059ab75dcf9eb864038dedd79f643c
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-03-31 21:37:29 +0530

    DC

---
+++ src/RestAssured_Test/StatuscodeValidation.java
@@ -0,0 +1,25 @@
+package RestAssured_Test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class StatuscodeValidation {
+
+	public static void main(String[] args) {
+		
+		//Status code validation
+		//200-OK
+		//201-Created
+		//204-No content
+		//400-Bad request
+		//401-Unauthorized
+		//403-Forbidden
+		//404-Not found
+		//500-Internal server error
+		
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+		
+
+	}
+
+}
