0612a7736ab32dacc84df162491b6242b3905067
mohanasundaram240100 <p.mohanasundaram@myyahoo.com>
2026-05-15 14:21:30 +0530

    DC

--- src/RestAssured_Test/BasicTests.java
+++ src/RestAssured_Test/BasicTests.java
@@ -1,14 +1,25 @@
 package RestAssured_Test;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 
 public class BasicTests {
 	
 	public static void main (String[] args) {
 		
-		//RestAssured.baseURI="https://reqres.in/api/users";
+		String response= given().formParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com").formParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+		.formParam("grant_type", "client_credentials").formParam("scope", "trust").when().post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").toString();
 		
+		System.out.println(response);
+		
+		JsonPath js = new JsonPath(response);
+		String accessToken= js.getString("access_token");
+		
+	
+		String CourseResponse= given().queryParam(accessToken).when().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").toString();
+		System.out.println(CourseResponse);
 		
 	}
 
